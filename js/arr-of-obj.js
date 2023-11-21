b0a3fea78946888f165508d347c3381a95eb547e
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-21 09:53:27 +0200

    filter find

---
+++ js/arr-of-obj.js
@@ -0,0 +1,35 @@
+'use strict';
+console.log('arr-of-obj.js file was loaded');
+
+const mainArr = [
+  {
+    name: 'Mickey Mouse',
+    age: 92,
+    gender: 'Male',
+    town: 'Disneyville',
+  },
+  {
+    name: 'SpongeBob SquarePants',
+    age: 33,
+    gender: 'Male',
+    town: 'Bikini Bottom',
+  },
+  {
+    name: 'Dora the Explorer',
+    age: 14,
+    gender: 'Female',
+    town: 'Playa Verde',
+  },
+  {
+    name: 'Bugs Bunny',
+    age: 83,
+    gender: 'Male',
+    town: 'Looneyville',
+  },
+  {
+    name: 'Lisa Simpson',
+    age: 9,
+    gender: 'Female',
+    town: 'Springfield',
+  },
+];
